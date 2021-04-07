10d1d295b82bdae531140192d2d6aa57cf422071
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2021-04-07 09:31:50 -0400

    Successful first tests

--- test_expressvpn.py
+++ test_expressvpn.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 from unittest.mock import patch
 import contents.exprvpnmon
 
@@ -6,23 +7,23 @@ class TestExpress(unittest.TestCase):
 
 
     def test_check_location(self):
-        self.assertEquals(contents.exprvpnmon.check_location('smart'), 'smart')
-        self.assertEquals(contents.exprvpnmon.check_location('usny'), 'usny')
-        self.assertEquals(contents.exprvpnmon.check_location('dub'), 'smart')
+        self.assertEqual(contents.exprvpnmon.check_location('smart'), 'smart')
+        self.assertEqual(contents.exprvpnmon.check_location('usny'), 'usny')
+        self.assertEqual(contents.exprvpnmon.check_location('dub'), 'smart')
 
     def test_conn_status(self):
         with patch('contents.exprvpnmon.subprocess.check_output') as mocked_output:
-            mocked_output = b'Connected to New Jersey'
-            self.assertEquals(contents.exprvpnmon.conn_status(), True)
+            mocked_output.return_value = b'Connected to New Jersey'
+            self.assertEqual(contents.exprvpnmon.conn_status(), True)
 
-            mocked_output = b'Connected and A new version'
-            self.assertEquals(contents.exprvpnmon.conn_status(), True)
+            mocked_output.return_value = b'Connected\nA new version'
+            self.assertEqual(contents.exprvpnmon.conn_status(), True)
 
-            mocked_output = b'Disconnected'
-            self.assertEquals(contents.exprvpnmon.conn_status(), False)
+            mocked_output.return_value = b'Disconnected'
+            self.assertEqual(contents.exprvpnmon.conn_status(), False)
 
-            mocked_output = b'Not found'
-            self.assertEquals(contents.exprvpnmon.conn_status(), False)
+            mocked_output.return_value = b'Not found'
+            self.assertEqual(contents.exprvpnmon.conn_status(), False)
 
 
 if __name__ == '__main__':
