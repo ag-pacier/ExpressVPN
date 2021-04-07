979d3bbae9040fa81c8a9e9550ff1712ac560a78
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2021-04-07 09:24:29 -0400

    Removed alias and blank test

--- test_expressvpn.py
+++ test_expressvpn.py
@@ -1,29 +1,28 @@
 import unittest
 from unittest.mock import patch
-import contents.exprvpnmon as source
+import contents.exprvpnmon
 
 class TestExpress(unittest.TestCase):
 
 
     def test_check_location(self):
-        self.assertEquals(source.check_location('smart'), 'smart')
-        self.assertEquals(source.check_location('usny'), 'usny')
-        self.assertEquals(source.check_location('dub'), 'smart')
-        self.assertEquals(source.check_location(), 'smart')
+        self.assertEquals(contents.exprvpnmon.check_location('smart'), 'smart')
+        self.assertEquals(contents.exprvpnmon.check_location('usny'), 'usny')
+        self.assertEquals(contents.exprvpnmon.check_location('dub'), 'smart')
 
     def test_conn_status(self):
-        with patch('exprvpnmon.subprocess.check_output') as mocked_output:
+        with patch('contents.exprvpnmon.subprocess.check_output') as mocked_output:
             mocked_output = b'Connected to New Jersey'
-            self.assertEquals(source.conn_status(), True)
+            self.assertEquals(contents.exprvpnmon.conn_status(), True)
 
             mocked_output = b'Connected and A new version'
-            self.assertEquals(source.conn_status(), True)
+            self.assertEquals(contents.exprvpnmon.conn_status(), True)
 
             mocked_output = b'Disconnected'
-            self.assertEquals(source.conn_status(), False)
+            self.assertEquals(contents.exprvpnmon.conn_status(), False)
 
             mocked_output = b'Not found'
-            self.assertEquals(source.conn_status(), False)
+            self.assertEquals(contents.exprvpnmon.conn_status(), False)
 
 
 if __name__ == '__main__':
